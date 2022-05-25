af899d3e4f4057101555c231fce28ad9f3691bb4
Yurii <Rapacta@gmail.com>
2022-05-26 00:56:46 +0200

    stask 1 comleted

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startСhangeColors = document.querySelector('button[data-start]');
+startСhangeColors.addEventListener('click', setTimeOut)
 
+const stopСhangeColors = document.querySelector('button[data-stop]');
+stopСhangeColors.addEventListener('click', stopInterval)
+
+const targetBody = document.body;
+
+let timerId
+
+function stopInterval() {
+  clearInterval(timerId);
+  startСhangeColors.disabled = false;
+}
+
+function setTimeOut() {
+  timerId = setInterval(onClick, 1000);
+  startСhangeColors.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onClick() {
+  targetBody.style.backgroundColor = getRandomHexColor();
+}
