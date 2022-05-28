ee2b85606d1eb3d1d1740124790080522330eb77
Yurii <Rapacta@gmail.com>
2022-05-28 23:48:50 +0200

    update hw-9

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -34,6 +34,7 @@ const options = {
 flatpickr(refs.input, options);
 
 function onClickBtn() {
+    refs.btn.disabled = true;
   intervalId = setInterval(() => {
     const meterTime = selectedDate - Date.now();
     const { days, hours, minutes, seconds } = convertMs(meterTime);
@@ -42,21 +43,24 @@ function onClickBtn() {
       clearInterval(intervalId);
       return;
     }
-    refs.btn.disabled = true;
+  
     updateTimer({ days, hours, minutes, seconds });
   }, 1000);
 }
 
+function addLeadingZero(value){
+    return String(value).padStart(2, '0');
+}
 function convertMs(ms) {
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
   return { days, hours, minutes, seconds };
 }
